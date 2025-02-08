33bd97940480d41364e8907d819d30aa3e59334b
Cali <renatoac82@gmail.com>
2025-02-08 16:44:33 -0300

    .

--- api/recommendation_api.py
+++ api/recommendation_api.py
@@ -45,7 +45,7 @@ app.add_middleware(
     CORSMiddleware,
      allow_origins=[
         "http://localhost:3000",
-        "https://mediamatchmaker.vercel.app/", 
+        "https://mediamatchmaker.vercel.app", 
     ],
     allow_credentials=True,
     allow_methods=["*"],
