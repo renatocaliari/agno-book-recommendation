d88ca684be7e0c6dc254318e6dfc418a3d6f12fd
Cali <renatoac82@gmail.com>
2025-02-08 16:50:00 -0300

    .

--- api/recommendation_api.py
+++ api/recommendation_api.py
@@ -44,8 +44,8 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
      allow_origins=[
-        "http://localhost:3000",
         "https://mediamatchmaker.vercel.app", 
+        "http://localhost:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
